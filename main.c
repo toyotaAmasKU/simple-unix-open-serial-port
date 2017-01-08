c5e93f6b77fe208a79936152d650f9f566dbe129
psychoAB <dome.2499@hotmail.com>
2017-01-08 23:30:13 +0700

    refactor: create set_port_atttribute function

--- main.c
+++ main.c
@@ -20,26 +20,9 @@ int open_serial_port(const char *dev)
     return file_description;
 }
 
-int main(int argc, char *argv[])
+int set_port_attribute(int file_description, int baud_rate)
 {
-    if(argc < 3)
-    {
-        return 1;
-    }
-
-    const char *dev = argv[1];
-    const char *baud_rate_string = argv[2];
-
     struct termios options;
-    int file_description, baud_rate;
-
-    file_description = open_serial_port(dev);
-    baud_rate = atoi(baud_rate_string);
-
-    if(file_description == -1)
-    {
-        return 1;
-    }
 
     tcgetattr(file_description, &options);
 
@@ -139,6 +122,34 @@ int main(int argc, char *argv[])
     options.c_iflag &= ~OPOST;
 
     tcsetattr(file_description, TCSANOW, &options);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 3)
+    {
+        return 1;
+    }
+
+    const char *dev = argv[1];
+    const char *baud_rate_string = argv[2];
+
+    int file_description, baud_rate;
+
+    file_description = open_serial_port(dev);
+    baud_rate = atoi(baud_rate_string);
+
+    if(file_description == -1)
+    {
+        return 1;
+    }
+
+    if(set_port_attribute(file_description, baud_rate))
+    {
+        return 1;
+    }
 
     close(file_description);
 

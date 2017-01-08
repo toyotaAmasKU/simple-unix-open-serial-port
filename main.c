ac6a22d0cea2e04a85c9ca9dc3177cf30515c6c2
psychoAB <dome.2499@hotmail.com>
2017-01-08 22:15:44 +0700

    can open serial port

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
+#include <fcntl.h>
+
 int main(int argc, char *argv[])
 {
+    if(argc < 2)
+    {
+        return 1;
+    }
+
+    const char *dev = argv[1];
+
+    int file_description;
+
+    file_description = open(dev, O_RDWR | O_NOCTTY | O_NDELAY);
+    
+    if(file_description == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        fcntl(file_description, F_SETFL, FNDELAY);
+    }
+
     return 0;
 }
